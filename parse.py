0714d0d4aaef201f4cae44165883a1fb7e130268
Eren Sulutas <erensulu11@gmail.com>
2019-04-28 15:04:42 -0400

    Update parse.py
    added extra tests with interesting results

--- parse.py
+++ parse.py
@@ -11,3 +11,12 @@ def print_link(link):
 
 # link to the Wikipedia main page
 print_link(urlparse('https://en.wikipedia.org/wiki/Main_Page'))
+
+# link to a random Wikipedia page
+print_link(urlparse('https://en.wikipedia.org/wiki/Special:Random'))
+
+# link to an amazon item page
+print_link(urlparse('https://www.amazon.ca/Star-Wars-Black-Skywalker-Lightsaber/dp/B017KIQOSQ/ref=lp_11992409011_1_13?srs=11992409011&ie=UTF8&qid=1556476876&sr=8-13'))
+
+# link to an amazon item through a search
+print_link(urlparse('https://www.amazon.ca/Black-OBI-Wan-Kenobi-Force-Lightsaber/dp/B07C7JKWST/ref=sr_1_6?keywords=black+series+lightsaber&qid=1556476935&s=gateway&sr=8-6'))
