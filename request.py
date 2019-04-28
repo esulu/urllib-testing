eff0abf2d9ac03473aa7e09c714382cb181336e3
Eren Sulutas <erensulu11@gmail.com>
2019-04-28 15:16:28 -0400

    Update request.py

--- request.py
+++ request.py
@@ -19,5 +19,5 @@ obj = urllib.request.Request(url='https://en.wikipedia.org/wiki/Main_Page',
 
 link2 = urllib.request.urlopen(obj)
 
-# prints the html of the web page as done in line 12, however formats it as well this time
+# prints the html of the web page as done in line 136, however formats it as well this time
 print(link2.read().decode('utf-8'))
